ed1baffce70fc41c19967d3afaed552d2ab2b33b
MelodyZhangHelloWorld <ge.zhang01@edu.sait.ca>
2021-03-24 18:48:53 -0400

    GET findAll

--- spring-boot-rest/src/main/java/com/webage/api/CustomerAPI.java
+++ spring-boot-rest/src/main/java/com/webage/api/CustomerAPI.java
@@ -1,9 +1,11 @@
 package com.webage.api;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.webage.domain.Customer;
 import com.webage.repository.CustomersRepository;
 
 @RestController
@@ -12,5 +14,12 @@ public class CustomerAPI {
 	
 	@Autowired
 	CustomersRepository repo;
+	
+	@GetMapping
+	public Iterable<Customer>getAll() {
+		return repo.findAll();		
+	}
+	
+	
 
 }
