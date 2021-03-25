84c84bd5daaf00ff48d361987208aa74d8591ba0
MelodyZhangHelloWorld <ge.zhang01@edu.sait.ca>
2021-03-24 20:23:15 -0400

    PUT1

--- spring-boot-rest/src/main/java/com/webage/api/CustomerAPI.java
+++ spring-boot-rest/src/main/java/com/webage/api/CustomerAPI.java
@@ -1,9 +1,18 @@
 package com.webage.api;
 
+import java.net.URI;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpRequest;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import com.webage.domain.Customer;
 import com.webage.repository.CustomersRepository;
@@ -19,6 +28,31 @@ public class CustomerAPI {
 	public Iterable<Customer>getAll() {
 		return repo.findAll();		
 	}
+	@GetMapping("/{customerId}")
+	public Customer getCustomerById (@PathVariable("customerId") long id) {
+		return repo.findOne(id);
+	}
+	
+	
+	
+	@PostMapping
+	public ResponseEntity <?> addCustomer (@RequestBody Customer newCustomer, HttpRequest request, UriComponentsBuilder uri){
+		if (newCustomer.getId() !=0 || newCustomer.getName() == null || newCustomer.getEmail() == null) {
+			//Reject - assign the customer id
+			return ResponseEntity.badRequest().build();
+		}
+		
+		
+		newCustomer = repo.save(newCustomer);
+		URI location = ServletUriComponentsBuilder.fromCurrentRequest()
+				.path("/{id}").buildAndExpand(newCustomer.getId()).toUri();
+		
+		ResponseEntity<?> response = ResponseEntity.created(location).build();
+			
+		return response;
+		
+	}
+	
 	
 	
 
